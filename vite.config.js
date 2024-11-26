134f60b179b399f1f4acd81d97293dff02dcb722
BlackiePearlJoobi <katze526reiswein@gmail.com>
2024-11-26 20:07:05 +0900

    update vite config base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/BlackiePearlJoobi/npm-experiment.git",
+  base: "https://github.com/BlackiePearlJoobi/npm-experiment",
 });
